7f670783682ae5bb5d7eb1b173814be7ceefce58
ryo kawamura <kawa1214mura@gmail.com>
2020-12-12 18:01:44 +0900

    apply webpack

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const outputPath = path.resolve(__dirname, "public");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.ts',
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devServer: {
+    contentBase: `${outputPath}/`,
+    open: true,
+    hot: true,
+    watchContentBase: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin()
+  ],
+  resolve: {
+    extensions: [
+      '.ts', '.js',
+    ],
+  },
+  target: ["web", "es5"],
+};
